7a6ed9e472642d00031d894bcdb7bf795750f536
Samuel Assis Vieira <samuelassis@jordao.grad.dcc.ufmg.br>
2019-10-31 16:19:07 -0300

    Começando CountingSort

--- TP2/src/functions.cpp
+++ TP2/src/functions.cpp
@@ -1,4 +1,19 @@
-//#include
+#include "functions.h"
+#include <string>
+#include <iostream>
 
 
-//void CountingSort()
+void CountingSort(int n, Planet main[], Planet result[]){
+	int bigger = main[0];
+	
+	for(int i=0; i<n;i++){
+		if(main[i] > bigger)
+			bigger = main[i];
+	}
+	int indexes[bigger];
+	
+	for(i=0;i<bigger;i++){
+		indexes[i] = 0;
+	}
+		
+}
